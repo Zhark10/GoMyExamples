1da7bfe83a54f36169bcae9c8563b5a606626a60
Zhark10 <verygoodman18@yandex.ru>
2022-04-19 17:36:20 +0300

    add main

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+import (
+	"fmt"
+	// "./user"
+	// "./cars" .
+	"./calc"
+)
+
+// func main() {
+// 	result := user.GetUser(user.GetIndexByUser, "Andrew")
+// 	fmt.Println(result)
+// }
+
+// func main() {
+// 	cars.CreateCars()
+// }
+
+func main() {
+	for i := 1; i < 7; i++{
+		go calc.Factorial(i)
+	}
+	fmt.Scanln()
+	fmt.Println("The End")
+}
+
+
